70c3c94df2b505ee1c6399cbcc2bf5ca206cb8c1
HannaInIT <ann10062016@gmail.com>
2023-09-12 12:10:16 +0200

    niial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <section>
+        <h1>this is the Header component</h1>
+        </section>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+function Main(){
+    return (
+        <section>
+        <h1>this is the Main component</h1>
+        </section>
+    )
+}
+
+export default Main;
