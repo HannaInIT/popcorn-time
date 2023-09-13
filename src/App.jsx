94d345a86e1a0528b044145b80e5c99536f0e1be
HannaInIT <ann10062016@gmail.com>
2023-09-13 11:37:07 +0200

    (lift state up) display nimber of movies in Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+
+import movies from "./data/movies.json";
+
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
@@ -5,10 +9,22 @@ import Footer from './components/Footer'
 import './App.css'
 
 function App() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  
+  const deleteMovie = (movieId) => {
+    const newList = moviesToDisplay.filter( (element) => {
+        return element.id !== movieId;
+    });
+    setMoviesToDisplay(newList);
+  }
+
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header numberOfMovies={moviesToDisplay.length} />
+      <Main movies={moviesToDisplay} callbackToDelete={deleteMovie} />
       <Footer />
     </>
   )
